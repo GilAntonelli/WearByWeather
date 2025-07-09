89a7b558ee548375f533ed04a9f4dd14a4b72fa7
Gilcimar Goncalves <gilcimar.goncalves.externo@ageas.pt>
2025-07-09 20:54:11 +0100

    [UPDATE] Refactor suggestionEngine

--- services/suggestionEngine.ts
+++ services/suggestionEngine.ts
@@ -25,12 +25,12 @@ export function getSuggestionByWeather({
   let recomendação = '';
   let image = null;
 
-  let suggestions = getSuggestions(genero, getThermalRangeDecription(tempAjustada), conforto, t);
+  let suggestions = getSuggestionsJson(genero, getThermalRangeDecription(tempAjustada), conforto, t);
   roupaSuperior = suggestions.roupaSuperior;
   roupaInferior = suggestions.roupaInferior;
   acessórios = suggestions.acessórios || [];
   recomendação = suggestions.recomendação;
-  image = getAvatar('frio', genero);
+  image = suggestions.image;
 
   // ☔ Chuva
   if (chuva) {
@@ -55,40 +55,102 @@ export function getSuggestionByWeather({
 
 // Map estático para evitar erro no Metro bundler
 const avatarMap: Record<string, any> = {
-  'frio_masculino': require('../assets/images/frio/AvatarMasculino.png'),
-  'frio_feminino': require('../assets/images/frio/AvatarFeminino.png'),
-  'frio_unissex': require('../assets/images/frio/AvatarUnissex.png'),
+  'freezing_male_feel_colder': require('../assets/avatars/freezing/male/feel_colder/avatar.png'),
+  'freezing_male_feel_hot': require('../assets/avatars/freezing/male/feel_hot/avatar.png'),
+  'freezing_male_neutral': require('../assets/avatars/freezing/male/neutral/avatar.png'),
+  'freezing_female_feel_colder': require('../assets/avatars/freezing/female/feel_colder/avatar.png'),
+  'freezing_female_feel_hot': require('../assets/avatars/freezing/female/feel_hot/avatar.png'),
+  'freezing_female_neutral': require('../assets/avatars/freezing/female/neutral/avatar.png'),
+  'freezing_unisex_feel_colder': require('../assets/avatars/freezing/unisex/feel_colder/avatar.png'),
+  'freezing_unisex_feel_hot': require('../assets/avatars/freezing/unisex/feel_hot/avatar.png'),
+  'freezing_unisex_neutral': require('../assets/avatars/freezing/unisex/neutral/avatar.png'),
+  
+  'cold_male_feel_colder': require('../assets/avatars/cold/male/feel_colder/avatar.png'),
+  'cold_male_feel_hot': require('../assets/avatars/cold/male/feel_hot/avatar.png'),
+  'cold_male_neutral': require('../assets/avatars/cold/male/neutral/avatar.png'),
+  'cold_female_feel_colder': require('../assets/avatars/cold/female/feel_colder/avatar.png'),
+  'cold_female_feel_hot': require('../assets/avatars/cold/female/feel_hot/avatar.png'),
+  'cold_female_neutral': require('../assets/avatars/cold/female/neutral/avatar.png'),
+  'cold_unisex_feel_colder': require('../assets/avatars/cold/unisex/feel_colder/avatar.png'),
+  'cold_unisex_feel_hot': require('../assets/avatars/cold/unisex/feel_hot/avatar.png'),
+  'cold_unisex_neutral': require('../assets/avatars/cold/unisex/neutral/avatar.png'),
 
-  'fresco_masculino': require('../assets/images/fresco/AvatarMasculino.png'),
-  'fresco_feminino': require('../assets/images/fresco/AvatarFeminino.png'),
-  'fresco_unissex': require('../assets/images/fresco/AvatarUnissex.png'),
+  'chilly_male_feel_colder': require('../assets/avatars/chilly/male/feel_colder/avatar.png'),
+  'chilly_male_feel_hot': require('../assets/avatars/chilly/male/feel_hot/avatar.png'),
+  'chilly_male_neutral': require('../assets/avatars/chilly/male/neutral/avatar.png'),
+  'chilly_female_feel_colder': require('../assets/avatars/chilly/female/feel_colder/avatar.png'),
+  'chilly_female_feel_hot': require('../assets/avatars/chilly/female/feel_hot/avatar.png'),
+  'chilly_female_neutral': require('../assets/avatars/chilly/female/neutral/avatar.png'),
+  'chilly_unisex_feel_colder': require('../assets/avatars/chilly/unisex/feel_colder/avatar.png'),
+  'chilly_unisex_feel_hot': require('../assets/avatars/chilly/unisex/feel_hot/avatar.png'),
+  'chilly_unisex_neutral': require('../assets/avatars/chilly/unisex/neutral/avatar.png'),
 
-  'ameno_masculino': require('../assets/images/ameno/AvatarMasculino.png'),
-  'ameno_feminino': require('../assets/images/ameno/AvatarFeminino.png'),
-  'ameno_unissex': require('../assets/images/ameno/AvatarUnissex.png'),
+  'mild_male_feel_colder': require('../assets/avatars/mild/male/feel_colder/avatar.png'),
+  'mild_male_feel_hot': require('../assets/avatars/mild/male/feel_hot/avatar.png'),
+  'mild_male_neutral': require('../assets/avatars/mild/male/neutral/avatar.png'),
+  'mild_female_feel_colder': require('../assets/avatars/mild/female/feel_colder/avatar.png'),
+  'mild_female_feel_hot': require('../assets/avatars/mild/female/feel_hot/avatar.png'),
+  'mild_female_neutral': require('../assets/avatars/mild/female/neutral/avatar.png'),
+  'mild_unisex_feel_colder': require('../assets/avatars/mild/unisex/feel_colder/avatar.png'),
+  'mild_unisex_feel_hot': require('../assets/avatars/mild/unisex/feel_hot/avatar.png'),
+  'mild_unisex_neutral': require('../assets/avatars/mild/unisex/neutral/avatar.png'),
 
-  'quente_masculino': require('../assets/images/quente/AvatarMasculino.png'),
-  'quente_feminino': require('../assets/images/quente/AvatarFeminino.png'),
-  'quente_unissex': require('../assets/images/quente/AvatarUnissex.png'),
+  'comfortable_male_feel_colder': require('../assets/avatars/comfortable/male/feel_colder/avatar.png'),
+  'comfortable_male_feel_hot': require('../assets/avatars/comfortable/male/feel_hot/avatar.png'),
+  'comfortable_male_neutral': require('../assets/avatars/comfortable/male/neutral/avatar.png'),
+  'comfortable_female_feel_colder': require('../assets/avatars/comfortable/female/feel_colder/avatar.png'),
+  'comfortable_female_feel_hot': require('../assets/avatars/comfortable/female/feel_hot/avatar.png'),
+  'comfortable_female_neutral': require('../assets/avatars/comfortable/female/neutral/avatar.png'),
+  'comfortable_unisex_feel_colder': require('../assets/avatars/comfortable/unisex/feel_colder/avatar.png'),
+  'comfortable_unisex_feel_hot': require('../assets/avatars/comfortable/unisex/feel_hot/avatar.png'),
+  'comfortable_unisex_neutral': require('../assets/avatars/comfortable/unisex/neutral/avatar.png'),
 
-  'calor_masculino': require('../assets/images/calor/AvatarMasculino.png'),
-  'calor_feminino': require('../assets/images/calor/AvatarFeminino.png'),
-  'calor_unissex': require('../assets/images/calor/AvatarUnissex.png'),
+  'warm_male_feel_colder': require('../assets/avatars/warm/male/feel_colder/avatar.png'),
+  'warm_male_feel_hot': require('../assets/avatars/warm/male/feel_hot/avatar.png'),
+  'warm_male_neutral': require('../assets/avatars/warm/male/neutral/avatar.png'),
+  'warm_female_feel_colder': require('../assets/avatars/warm/female/feel_colder/avatar.png'),
+  'warm_female_feel_hot': require('../assets/avatars/warm/female/feel_hot/avatar.png'),
+  'warm_female_neutral': require('../assets/avatars/warm/female/neutral/avatar.png'),
+  'warm_unisex_feel_colder': require('../assets/avatars/warm/unisex/feel_colder/avatar.png'),
+  'warm_unisex_feel_hot': require('../assets/avatars/warm/unisex/feel_hot/avatar.png'),
+  'warm_unisex_neutral': require('../assets/avatars/warm/unisex/neutral/avatar.png'),
+
+  'hot_male_feel_colder': require('../assets/avatars/freezing/male/feel_colder/avatar.png'),
+  'hot_male_feel_hot': require('../assets/avatars/freezing/male/feel_hot/avatar.png'),
+  'hot_male_neutral': require('../assets/avatars/freezing/male/neutral/avatar.png'),
+  'hot_female_feel_colder': require('../assets/avatars/freezing/female/feel_colder/avatar.png'),
+  'hot_female_feel_hot': require('../assets/avatars/freezing/female/feel_hot/avatar.png'),
+  'hot_female_neutral': require('../assets/avatars/freezing/female/neutral/avatar.png'),
+  'hot_unisex_feel_colder': require('../assets/avatars/freezing/unisex/feel_colder/avatar.png'),
+  'hot_unisex_feel_hot': require('../assets/avatars/freezing/unisex/feel_hot/avatar.png'),
+  'hot_unisex_neutral': require('../assets/avatars/freezing/unisex/neutral/avatar.png'),
+
+  'extreme_heat_male_feel_colder': require('../assets/avatars/extreme_heat/male/feel_colder/avatar.png'),
+  'extreme_heat_male_feel_hot': require('../assets/avatars/extreme_heat/male/feel_hot/avatar.png'),
+  'extreme_heat_male_neutral': require('../assets/avatars/extreme_heat/male/neutral/avatar.png'),
+  'extreme_heat_female_feel_colder': require('../assets/avatars/extreme_heat/female/feel_colder/avatar.png'),
+  'extreme_heat_female_feel_hot': require('../assets/avatars/extreme_heat/female/feel_hot/avatar.png'),
+  'extreme_heat_female_neutral': require('../assets/avatars/extreme_heat/female/neutral/avatar.png'),
+  'extreme_heat_unisex_feel_colder': require('../assets/avatars/extreme_heat/unisex/feel_colder/avatar.png'),
+  'extreme_heat_unisex_feel_hot': require('../assets/avatars/extreme_heat/unisex/feel_hot/avatar.png'),
+  'extreme_heat_unisex_neutral': require('../assets/avatars/extreme_heat/unisex/neutral/avatar.png'),
 };
 
-function getAvatar(faixa: string, genero: string): any {
-  const key = `${faixa}_${genero}`;
+function getAvatar(faixa: string, genero: string, conforto: string): any {
+  const key = `${faixa}_${genero}_${conforto}`;
+  console.log('key:', key);
+  console.log('avatar:', avatarMap);
   return avatarMap[key] || avatarMap['ameno_masculino'];
 }
 
-export function getSuggestions(
+export function getSuggestionsJson(
   gender: string,
-  range: string,
+  rangeDescription: string,
   comfort: string,
   t: TFunction
 ): LookSuggestion {
 
-  let suggestions = t(`suggestions.${gender}.${range}.neutral`, {
+  let suggestions = t(`suggestions.${gender}.${rangeDescription}.${comfort}`, {
     returnObjects: true,
   }) as LookSuggestionJson;
   
@@ -97,7 +159,7 @@ export function getSuggestions(
     roupaInferior: suggestions.bottom || '',
     acessórios: suggestions.accessories || [],
     recomendação: suggestions.recommendation || '',
-    image: getAvatar(range, gender),
+    image: getAvatar(rangeDescription, gender, comfort),
   };
 }
 
